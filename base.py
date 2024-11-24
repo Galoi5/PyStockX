0b4a534b8bed4304cef74fc13f33c48a33fd97a2
Celian <celian.fabas@gmail.com>
2024-11-24 13:42:09 +0100

    first

---
+++ base.py
@@ -0,0 +1,2 @@
+class StockX():
+    pass
---
+++ batch.py
@@ -0,0 +1,37 @@
+class Batch():
+    def __init__(self):
+        pass
+
+    class Create():
+        
+        def Listings(self):
+            pass
+
+        def Status(self):
+            pass
+
+        def Items(self):
+            pass
+
+    class Delete():
+
+        def Listings(self):
+            pass
+        
+        def Status(self):
+            pass
+
+        def Items(self):
+            pass
+
+    class Update():
+
+        def Listings():
+            pass
+
+        def Status():
+            pass
+
+        def Items():
+            pass
+        
